a6b6ec8b0121374940f0549c6338fbddeaf054bc
WinnerOK <danman339@gmail.com>
2019-12-07 14:36:14 +0300

    day 3 part 2

--- day 3/main.go
+++ day 3/main.go
@@ -14,10 +14,10 @@ type point struct {
 	y int
 }
 
-func pointSum(p1, p2 point) point {
+func pointSum(p1 point, x, y int) point {
 	return point{
-		x: p1.x + p2.x,
-		y: p1.y + p2.y,
+		x: p1.x + x,
+		y: p1.y + y,
 	}
 }
 
@@ -36,53 +36,12 @@ func segmentsIntersection(s1, s2 lineSegment) *geos.Geometry {
 	return geos.Must(line1.Intersection(line2))
 }
 
-func parseWire(wire []string) []lineSegment {
-	var result []lineSegment
-	current := point{
-		x: 0,
-		y: 0,
-	}
-	var delta point
-
-	for _, step := range wire {
-		num, _ := strconv.Atoi(step[1:])
-		switch step[0] {
-		case 'U':
-			delta = point{
-				y: num,
-			}
-		case 'D':
-			delta = point{
-				y: -num,
-			}
-		case 'R':
-			delta = point{
-				x: num,
-			}
-		case 'L':
-			delta = point{
-				x: -num,
-			}
-		}
-		result = append(result, lineSegment{
-			begin: current,
-			end:   pointSum(current, delta),
-		})
-
-		current = pointSum(current, delta)
-
-	}
-
-	return result
-}
-
 func findAllIntersections(layout1, layout2 []lineSegment) []point {
 	var result []point
 	for _, s1 := range layout1 {
 		for _, s2 := range layout2 {
 			intersection := segmentsIntersection(s1, s2)
 			if b, _ := intersection.IsEmpty(); !b {
-				//fmt.Printf("Intersection: %v %v\n", s1, s2)
 				x, _ := intersection.X()
 				y, _ := intersection.Y()
 				result = append(result, point{
@@ -96,26 +55,86 @@ func findAllIntersections(layout1, layout2 []lineSegment) []point {
 	return result
 }
 
-func rank(intersections []point) {
-	sort.Slice(intersections, func(i, j int) bool {
-		return math.Abs(float64(intersections[i].x))+math.Abs(float64(intersections[i].y)) < math.Abs(float64(intersections[j].x))+math.Abs(float64(intersections[j].y))
-	})
+func splitWireToSegments(wire []string) []lineSegment {
+	var wireSegments []lineSegment
+	current := point{
+		x: 0,
+		y: 0,
+	}
+	dx := map[string]int{"U": 0, "D": 0, "R": 1, "L": -1}
+	dy := map[string]int{"U": 1, "D": -1, "R": 0, "L": 0}
 
-	fmt.Printf("%v\nAnswer for part 1: %d", intersections, intersections[1].y+intersections[1].x)
+	for _, step := range wire {
+		num, _ := strconv.Atoi(step[1:])
+		direction := step[:1]
+		newPoint := pointSum(current, dx[direction]*num, dy[direction]*num)
+
+		wireSegments = append(wireSegments, lineSegment{
+			begin: current,
+			end:   newPoint,
+		})
+		current = newPoint
+	}
+
+	return wireSegments
 }
 
-func part1(wire1, wire2 []lineSegment) {
-	rank(findAllIntersections(wire1, wire2))
+func makeWireDistMap(wirePlan []string) map[point]int {
+	distMap := map[point]int{}
+	current := point{
+		x: 0,
+		y: 0,
+	}
+	dx := map[string]int{"U": 0, "D": 0, "R": 1, "L": -1}
+	dy := map[string]int{"U": 1, "D": -1, "R": 0, "L": 0}
+
+	stepsDone := 0
+
+	for _, move := range wirePlan {
+		direction := move[:1]
+		step, _ := strconv.Atoi(move[1:])
+
+		for i := 0; i < step; i++ {
+			current = pointSum(current, dx[direction], dy[direction])
+			stepsDone += 1
+
+			if _, ok := distMap[current]; !ok {
+				distMap[current] = stepsDone
+			}
+		}
+
+	}
+
+	return distMap
 }
 
-//All intersections
-//[{0 0} {258 0} {433 0} {433 408} {639 408} {990 153} {738 408} {496 683} {731 683} {890 708} {890 754} {890 915} {890 1208} {890 1260} {939 1367} {1166 1367} {1327 1787} {1327 1848} {1327 1888} {1327 1942} {1938 1348} {1327 1961} {1938 1454} {1327 2147} {2242 1348} {2194 1454} {2400 1348} {2290 1469} {2290 1474} {1435 2340} {2290 1530} {2744 1188} {1435 2757} {2290 2018} {3272 1220} {2419 2151} {2371 2231} {3272 1352} {2065 2757} {1389 3807} {2862 2719} {2727 2855} {2727 2990} {3026 2719} {3031 2719} {2104 3700} {2119 3700} {2119 3779} {2478 3463} {2478 3499} {2119 3988} {2119 3996} {3476 2719} {2308 3999} {2313 3999} {2906 3463} {3240 3670} {3714 3889} {4093 3912} {4888 3326} {5581 3167} {5702 3178} {5702 3531}]
+func CalculateMinTotalLengthIntersection(wire1, wire2 map[point]int, intersections []point) int {
+	min := math.MaxInt64
+	for _, v := range intersections {
+		dist := wire1[v] + wire2[v]
+		if dist < min {
+			min = dist
+		}
+	}
+	return min
+}
 
 func main() {
 	input := readInput("./in.txt")
-	wire1 := parseWire(strings.Split(input[0], ","))
-	wire2 := parseWire(strings.Split(input[1], ","))
+	wire1DistMap := makeWireDistMap(strings.Split(input[0], ","))
+	wire2DistMap := makeWireDistMap(strings.Split(input[1], ","))
 
-	part1(wire1, wire2)
+	wire1Segments := splitWireToSegments(strings.Split(input[0], ","))
+	wire2Segments := splitWireToSegments(strings.Split(input[1], ","))
+
+	intersections := findAllIntersections(wire1Segments, wire2Segments)
+	sort.Slice(intersections, func(i, j int) bool {
+		return math.Abs(float64(intersections[i].x))+math.Abs(float64(intersections[i].y)) <
+			math.Abs(float64(intersections[j].x))+math.Abs(float64(intersections[j].y))
+	})
+	fmt.Printf("Answer for part 1: %d\n", intersections[1].y+intersections[1].x)
+
+	fmt.Printf("Answer for part 2: %d\n",
+		CalculateMinTotalLengthIntersection(wire1DistMap, wire2DistMap, intersections[1:]))
 
 }
